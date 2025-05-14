8451b3ea876e4bc28affba3ed57da1b08808fa80
petitavo <gustavohuanca1020@gmail.com>
2025-05-14 18:31:42 -0500

    test: auth signin

---
+++ IAMService.Tests/Features/US25_Autenticacion.feature
@@ -0,0 +1,11 @@
+﻿Feature: Autenticación con credenciales válidas (US25)
+
+  Escenario: Autenticación exitosa
+  Given que el usuario tiene credenciales válidas
+  When hace un POST a "/api/v1/authentication/sign-in"
+  Then el sistema responde con 200 OK
+
+  Escenario: Autenticación fallida
+  Given que el usuario tiene credenciales inválidas
+  When hace un POST a "/api/v1/authentication/sign-in"
+  Then el sistema responde con 401 Unauthorized
---
+++ IAMService.Tests/Steps/US25_AutenticacionSteps.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Text;
+using RestSharp;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace IAMService.Tests.Steps;
+
+[Binding]
+public class US25_AutenticacionSteps
+{
+    private RestResponse? _response;
+
+    [When(@"hace un POST a ""(.*)""")]
+    public async Task WhenHacePost(string endpoint)
+    {
+        var client = new RestClient("http://localhost:5161");
+        var request = new RestRequest(endpoint, Method.Post);
+
+        if (ScenarioContext.Current.ScenarioInfo.Title.Contains("exitosa"))
+        {
+            request.AddJsonBody(new
+            {
+                email = "usuario@prueba.com",
+                password = "contrasenaCorrecta123"
+            });
+        }
+        else
+        {
+            request.AddJsonBody(new
+            {
+                email = "usuario@prueba.com",
+                password = "incorrecta"
+            });
+        }
+
+        _response = await client.ExecuteAsync(request);
+    }
+
+    [Then(@"el sistema responde con (\d{3}) OK")]
+    public void ThenRespuestaOk(int statusCode)
+    {
+        Assert.Equal((HttpStatusCode)statusCode, _response!.StatusCode);
+    }
+
+    [Then(@"el sistema responde con (\d{3}) Unauthorized")]
+    public void ThenRespuestaUnauthorized(int statusCode)
+    {
+        Assert.Equal((HttpStatusCode)statusCode, _response!.StatusCode);
+    }
+
+    [Given(@"que el usuario tiene credenciales válidas")]
+    public void GivenUsuarioConCredencialesValidas() { }
+
+    [Given(@"que el usuario tiene credenciales inválidas")]
+    public void GivenUsuarioConCredencialesInvalidas() { }
+}
