5ef7a1023a0726982b002b42eb4fbb24ed9cbce6
AngeloC12 <angelocuri@hotmail.com>
2025-05-14 15:48:07 -0500

    feat: added Lot Resource

---
+++ BURUBERI.InventoryService.API/Interface/REST/Resources/UpdateLoteResource.cs
@@ -0,0 +1,17 @@
+﻿namespace BURUBERI.InventoryService.API.Interface.REST.Resources;
+
+/// <summary>
+/// Resource for updating an existing Lot.
+/// </summary>
+public class UpdateLoteResource
+{
+    public string Type { get; set; }
+    public double WeightKg { get; set; }
+    public decimal UnitPrice { get; set; }
+    public string Quality { get; set; }
+    public string Status { get; set; }
+    public double Stock { get; set; }
+    public string LotNumber { get; set; }
+    public DateTime ProducedAt { get; set; }
+    public DateTime ExpiresAt { get; set; }
+}
