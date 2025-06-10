493054c9b8599a6123b290c33fa5e07324dfb5e3
petitavo <gustavohuanca1020@gmail.com>
2025-06-09 22:33:58 -0500

    fix

--- AdminService.API/obj/Debug/net8.0/AdminService.API.AssemblyInfo.cs
+++ AdminService.API/obj/Debug/net8.0/AdminService.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminService.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8451b3ea876e4bc28affba3ed57da1b08808fa80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c7ba672b978a3881965c2eb5dde76959bceaaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminService.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminService.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
